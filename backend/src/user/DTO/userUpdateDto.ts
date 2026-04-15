e9ea6fe941f4dddf79a7c55acd565e121ce012a3
Froxick <nvorozhevv@mail.ru>
2026-04-15 22:55:26 +0500

    UserUpdateDto validation

--- backend/src/user/DTO/userUpdateDto.ts
+++ backend/src/user/DTO/userUpdateDto.ts
@@ -1,4 +1,19 @@
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class UserUpdateDto {
+  @IsOptional()
+  @IsString({ message: 'Имя должно быть строкой' })
+  @MinLength(2, { message: 'Имя должно содержать минимум 2 символа' })
+  @MaxLength(15, { message: 'Имя должно содержать максимум 15 символов' })
   name?: string;
+  @IsOptional()
+  @IsString()
+  @IsEmail()
   email?: string;
 }
